2b025899a0b07c89f3d5074604d89daebc24886b
Nikola Zivojinovic <zivojinovicn13@gmail.com>
2024-01-17 18:03:32 +0100

    Added sharp package for better image optimization, Card component style changes

--- src/app/advice/page.tsx
+++ src/app/advice/page.tsx
@@ -2,8 +2,10 @@ import './AdvicePage.scss';
 
 import heroBackground from 'public/images/hero-background.webp';
 import Image from 'next/image';
+import AdviceIcon2 from '../_components/icons/AdviceIcon2';
+import AdviceIcon1 from '../_components/icons/AdviceIcon1';
+import AdviceIcon3 from '../_components/icons/AdviceIcon3';
 import { Card } from '../_components/Card';
-import { adviceItems } from '../_utils/content/adviceItems';
 
 export default function Page() {
 	return (
@@ -13,9 +15,12 @@ export default function Page() {
 				placeholder="blur"
 				fill
 				sizes="100vw"
+				priority
+				quality={50}
 				style={{
 					objectFit: 'cover',
-					zIndex: -1
+					zIndex: -1,
+					filter: 'brightness(50%)'
 				}}
 				alt="Background"
 				className="AdvicePage__image"
@@ -24,9 +29,24 @@ export default function Page() {
 			<h5 className="AdvicePage__subTitle">Sve što biste trebali znati za uspešnu selidbu.</h5>
 
 			<div className="AdvicePage__content">
-				{adviceItems.map((item, key) => (
-					<Card key={key} title={item.title} desc={item.description} icon={item.icon} link="" />
-				))}
+				<Card
+					title={'Planiranje'}
+					desc={`Napravite detaljan inventar: Pregledajte sve svoje stvari i napravite popis inventara kako biste znali šta tačno treba da se seli. Obezbedite kontakt s firmom za selidbe kako biste se usaglasili oko svih detalja. Informišite ih o posebnim predmetima koje treba preseliti i postavite pitanja o njihovom postupku kako biste bili sigurni da su svi detalji pokriveni.`}
+					icon={<AdviceIcon1 />}
+					link=""
+				/>
+				<Card
+					title={'Priprema'}
+					desc={`Priprema je ključna za sigurnu selidbu. Pre selidbe, očistite i organizujte svoje stvari, izbacujući sve što vam više nije potrebno.`}
+					icon={<AdviceIcon2 />}
+					link=""
+				/>
+				<Card
+					title={'Organizacija za dan selidbe'}
+					desc={`Organizacija na dan selidbe je ključna za glatku tranziciju. Osigurajte da su putevi slobodni kako biste olakšali transport kutija i nameštaja.`}
+					icon={<AdviceIcon3 />}
+					link=""
+				/>
 			</div>
 		</div>
 	);
